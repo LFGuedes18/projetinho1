7f381991eef172dc94b6cc4dd1b2e97683b07dac
LFGuedes18 <luiz.guedes@arapiraca.ufal.br>
2025-03-19 09:40:27 -0300

    update1903

--- app.py
+++ app.py
@@ -7,15 +7,6 @@ from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.uix.popup import Popup
-
-import datetime
-from plyer import notification
-from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.label import Label
-from kivy.uix.textinput import TextInput
-from kivy.uix.button import Button
-from kivy.uix.popup import Popup
 from kivy.uix.scrollview import ScrollView
 from kivy.uix.gridlayout import GridLayout
 
